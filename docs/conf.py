1122d55d0914454405ad6c31f2ccb14b1db7aa1b
Adalyia <45367157+Adalyia@users.noreply.github.com>
2022-02-21 15:23:34 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,13 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+source_suffix = {
+    ".rst": "restructuredtext"
+}
+
+master_doc = "index"
+
+exclude_patterns = ["_build"]
 
 # -- Options for HTML output -------------------------------------------------
 
